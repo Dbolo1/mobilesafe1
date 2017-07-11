7dd4ab744e46465338b63dfd3b66e93fdee62ddb
Dbolo1 <879160616@qq.com>
2017-07-12 07:56:52 +0800

    第一次提交

---
+++ app/src/main/java/com/bolo1/mobilesafe1/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.bolo1.mobilesafe1.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bolo1.mobilesafe1.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+    }
+}
